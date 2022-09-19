6daf92bd06e4acec6db5af22a4bbf0e979a5f166
Taump <taump@yandex.ru>
2022-09-19 22:15:46 +0400

    fix explorer links

--- src/utils/getExplorerLink.js
+++ src/utils/getExplorerLink.js
@@ -1,3 +1,5 @@
+import obyte from "obyte";
+
 import config from "appConfig";
 
 export const getExplorerLink = (network, data, type) => {
@@ -5,9 +7,15 @@ export const getExplorerLink = (network, data, type) => {
 
   if (!(environment === "testnet" || environment === "mainnet")) return data
 
+  let isObyteAddress = false;
+
+  if (network === "Obyte" && (type === 'address' || obyte.utils.isValidAddress(data))) {
+    isObyteAddress = true;
+  }
+
   switch (network) {
     case 'Obyte':
-      return environment === "testnet" ? `https://testnetexplorer.obyte.org/#${data}` : `https://explorer.obyte.org/#${data}`;
+      return environment === "testnet" ? `https://testnetexplorer.obyte.org/${isObyteAddress ? 'address/' : ''}${data}` : `https://explorer.obyte.org/${isObyteAddress ? 'address/' : ''}${data}`;
     case 'Ethereum':
       return environment === "testnet" ? `https://rinkeby.etherscan.io/${type || "tx"}/${data}` : `https://etherscan.io/${type || "tx"}/${data}`;
     case 'BSC':
