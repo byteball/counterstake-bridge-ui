e780a872b746b7231a2c5d8c9e791dd6cd03fffc
Taump <taump@yandex.ru>
2024-08-06 18:44:39 +0400

    chore: update testnet multicall address for BSC and Polygon

--- src/utils/getMulticallAddress.js
+++ src/utils/getMulticallAddress.js
@@ -7,15 +7,15 @@ const multiCalls = {
     },
     BSC: {
         mainnet: "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb",
-        testnet: "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb"
+        testnet: "0xa6949b8fba9df546b9c66f98cfca960a96e3b68e"
     },
     Polygon: {
         mainnet: "0x275617327c958bD06b5D6b871E7f491D76113dd8",
-        testnet: "0xca11bde05977b3631167028862be2a173976ca11", // mumbai
+        testnet: "0xe9939e7Ea7D7fb619Ac57f648Da7B1D425832631", // mumbai
     },
     Kava: {
         mainnet: "0x30A62aA52Fa099C4B227869EB6aeaDEda054d121",
-        testnet: "0x30A62aA52Fa099C4B227869EB6aeaDEda054d121"
+        testnet: "0x2EF217760a2694D4cb51697BD8BC4F3eC8221E7b"
     }
 }
 
