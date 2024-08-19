588c2d522f61d7357d6624e772d225b7fa8252d6
Taump <taump@yandex.ru>
2024-08-19 20:23:57 +0400

    fix(generateLink): use template strings

--- src/utils/generateLink.js
+++ src/utils/generateLink.js
@@ -10,15 +10,10 @@ const suffix = suffixes[config.ENVIRONMENT];
 
 export const generateLink = ({ amount, amount2, data, from_address, aa, asset, asset2, is_single }) => {
   let link = `obyte${suffix}:${aa}?amount=${Math.round(amount)}&asset=${encodeURIComponent(asset || "base")}`;
-  if (data)
-    link += '&base64data=' + encodeURIComponent(encodeData(data));
-  if (from_address)
-    link += '&from_address=' + from_address;
-  if (asset2)
-    link += '&asset2=' + encodeURIComponent(asset2);
-  if (amount2)
-    link += '&amount2=' + amount2;
-  if (is_single)
-    link += '&single_address=1';
+  if (data) link += `&base64data=${encodeURIComponent(encodeData(data))}`;
+  if (from_address) link += `&from_address=${from_address}`;
+  if (asset2) link += `&asset2=${encodeURIComponent(asset2)}`;
+  if (amount2) link += `&amount2=${amount2}`;
+  if (is_single) link += '&single_address=1';
   return link;
 };
