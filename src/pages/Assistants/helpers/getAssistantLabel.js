83ee42fe9ccca3c5621e405338ff977fa7d1fecc
Taump <taump@yandex.ru>
2024-08-19 20:19:25 +0400

    fix label

--- src/pages/Assistants/helpers/getAssistantLabel.js
+++ src/pages/Assistants/helpers/getAssistantLabel.js
@@ -1,5 +1,5 @@
 export const getAssistantLabel = (direction) => {
-    if (!direction) return "Unknown";
+    if (!direction || !direction.src_token || !direction.dst_token) return "Invalid direction";
     const symbol = direction.type === "expatriation" ? direction.src_token.symbol : direction.dst_token.symbol;
     return `${symbol}: ${direction.src_token.network} -> ${direction.dst_token.network} (${direction.dst_token.network} side)`;
 }
