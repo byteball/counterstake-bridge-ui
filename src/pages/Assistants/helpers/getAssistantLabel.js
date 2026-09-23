214465fe7fd924f4f42be210d15b2848b28d4656
Alexander Ponomarev <taump@yandex.ru>
2026-09-23 18:45:07 +0200

    fix assistant label generation logic

--- src/pages/Assistants/helpers/getAssistantLabel.js
+++ src/pages/Assistants/helpers/getAssistantLabel.js
@@ -2,6 +2,10 @@ export const getAssistantLabel = (direction) => {
     if (!direction || !direction.src_token || !direction.dst_token) {
         return "Invalid direction";
     }
-    const { src_token, dst_token } = direction;
-    return `${dst_token.symbol} on ${dst_token.network} -> ${src_token.symbol} on ${src_token.network} (${dst_token.network} side)`;
+
+    const { type, src_token, dst_token } = direction;
+
+    const [home_token, foreign_token] = type === "expatriation" ? [src_token, dst_token] : [dst_token, src_token];
+
+    return `${home_token.symbol} on ${home_token.network} -> ${foreign_token.symbol} on ${foreign_token.network} (${dst_token.network} side)`;
 }
